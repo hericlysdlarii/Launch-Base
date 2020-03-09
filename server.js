597650e60f3513bb090c6e5909d945bd70447763
hericlysdlarii <hericlysdlarii@gmail.com>
2020-03-09 17:03:32 -0300

    modal

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const nunjucks = require('nunjucks')
 const server = express()
 
 server.use(express.static('public'))
+server.use(express.static('media'))
 
 server.set("view engine", "html")
 
@@ -17,6 +18,10 @@ server.get("/", function(req, res){
 
 server.get("/portfolio", function(req, res){
   return res.render("portfolio")
+})
+
+server.get("/hobbies", function(req, res){
+  return res.render("hobbies")
 })
 
 server.listen(5000, function(){
